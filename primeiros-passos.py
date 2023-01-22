8d6690338161a9b764cf2631b58b5e9bbdf4ada5
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-01-22 00:41:27 -0300

    teste1

---
+++ primeiros-passos.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------
+#função (objeto) '' simbolizam strings
+print('Olá, Mundo!')
+print (7+4)
+print ('7'+'4')
+print('Olá, ', 10)
+#-------------------------------------------------------------------
+#No Python, toda variável é um objeto
+
+nome = 'Matheus'
+idade = 33
+peso = 75.8
+print(nome, idade, peso, 'kg')
+#-------------------------------------------------------------------
+#input significa leia
+nome = input('Qual é o seu nome? ')
+idade = input('Quantos anos você tem? ')
+peso = input('Qual é o seu peso? ')
+print(nome, idade, peso, 'kg')
+#-------------------------------------------------------------------
+#
---
+++ scripts-python/teste-01.py
@@ -0,0 +1,19 @@
+'''Crie um scrpt python, que leia o nome de uma pessoa
+e mostre a mensagem de boas-vindas de acordo com o valor digitado'''
+nome = input('Qual é o seu nome? ')
+print ('Olá, ', nome, 'seja bem-vindo ao Python!' )
+#-------------------------------------------------------------------
+'''Crie um script Python que leia o mês do ano de nascimento de uma
+pessoa e mostre uma mensagem com a data formatada'''
+ano = input('Qual ano você nasceu? ')
+mes = input('Qual mês você nasceu?? ')
+dia = input('Que dia você nasceu? ')
+print ('Você nasceu em: ', dia,'/', mes,'/', ano)
+#-------------------------------------------------------------------
+print ('Você nasceu no dia ', dia, 'de ', mes, 'de' , ano, '. Correto?')
+#-------------------------------------------------------------------
+'''Crie um script Python que leia dois numeros e tente mostrar a soma entre eles'''
+num1 = input('Digite o primero numero da soma desejeda: ')
+num2 = input('Digite o segundo numero da soma desejeda: ')
+num3 = (num1 + num2)
+print(num1, '+', num2, '= ', num3)
