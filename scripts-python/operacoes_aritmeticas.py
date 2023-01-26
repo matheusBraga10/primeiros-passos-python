4ce062097d7ae7eda94f7c25258e6d23679c1497
Matheus Felipe Braga <matheusfbraga10@gmail.com>
2023-01-25 23:17:57 -0300

    25-01

---
+++ scripts-python/operacoes_aritmeticas.py
@@ -0,0 +1,19 @@
+'''Operações Aritméticas
+
+    + Adição 5 + 2 == 7              
+    - Subtração 5 - 2 == 3             
+    / Divisão 5 / 2 == 2.5
+    * Multiplicação 5 * 2 == 10         
+    ** Potência 5 ** 2 == 25
+    // Divisão Inteira 5 // 2 == 2
+    % Resto da Divisão 5 % 2 == 1
+
+    Assim como na matemática, a ordem de procedência se inicia com 
+    1. ()
+    2. **
+    3. *, /, //, %
+    4. +, -'''
+    
+print(5+3*2) #Vide, ordem de procedência acima
+print(3*5+4**2)   
+print(3*(5+4)**2)
